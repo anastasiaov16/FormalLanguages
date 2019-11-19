153b4c8587a26bf7285834522a3d955536e0b0cd
anastasiaov16 <anastasiaov1998@mail.ru>
2019-11-19 21:53:00 +0400

    Task 1 attempt number 4

---
+++ Automation1/Automation1/Automate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Automation1
+{
+    public class Automate
+    {
+        Dictionary<string, Dictionary<string, string>> Transition = new Dictionary<string, Dictionary<string, string>>();
+        public string[] State;
+        public string[] Alph;
+        public string [] Start;
+        public string [] Stop;
+        
+        public string[] line = File.ReadAllLines("Automate.txt");
+
+        public Automate()
+        {
+            State = line[0].Split(',');
+            Alph = line[1].Split(',');
+            Start = line[line.Length - 2].Split(',');
+            Stop = line[line.Length - 1].Split(',');
+
+            for (var i = 2; i < line.Length - 2; i++)
+            {
+                var str2 = line[i].Split(':');
+
+                if (str2.Length == 1)
+                {
+                    string k = "";
+                    Dictionary<string, string> temp = new Dictionary<string, string>();
+                    for (var j = i + 1; j < i + 10; j++)
+                    {
+                        var strj = line[j].Split(':');
+                        temp.Add(strj[0], strj[1]);
+                    }
+                    k = String.Join("", str2);
+                    Transition.Add(k, temp);
+                    i += 9;
+                }
+            }
+        }
+
+        public KeyValuePair<bool, int> MaxString(string text, int startIndex)
+        {
+            bool flag = false;
+            int maxLength = 0;
+            int idx = startIndex;
+            string currentStates = String.Join("", Start);
+            
+            if (Stop.Contains(currentStates.ToString()))
+            {
+                flag = true;
+            }
+
+            for (int i = idx; i < text.Length; i++)
+            {
+                if (Alph.Contains(text[i].ToString()) && currentStates!= " ")
+                {
+                    foreach (var item in Transition)
+                    {
+                        if (item.Key == text[i].ToString())
+                        {
+                            foreach (var item1 in item.Value)
+                            {
+                                if(item1.Key == currentStates)
+                                {
+                                    currentStates=item1.Value;
+                                    goto Check;
+                                }
+                            }
+                        }
+                    }
+                    Check: if (Stop.Contains(currentStates.ToString()))
+                    {
+                        flag = true;
+                        maxLength = i + 1 - idx;
+
+                    }
+                }
+                else
+                {
+                    return new KeyValuePair<bool, int>(flag, maxLength);
+                }
+            }
+            return new KeyValuePair<bool, int>(flag, maxLength);
+        }
+
+        private static int CompareByLength(string a, string b)
+        {
+            if (a.Length > b.Length)
+                return 1;
+            if (a.Length == b.Length)
+                return 0;
+            else return -1;
+        }
+
+        public void Show(List<string> lst)
+        {
+            Console.WriteLine("Найдено слов: ");
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+
+            lst.Sort(CompareByLength);
+            Console.WriteLine("Слово максимальной длины: ");
+            Console.WriteLine(lst[lst.Count - 1]);
+
+        }
+    }
+}
+
+
---
+++ Automation1/Automation1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Automation1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> result = new List<string>();
+            StreamReader f = new StreamReader("input.txt", Encoding.Default);
+            string str = f.ReadToEnd();
+            Automate auto = new Automate(); 
+            int k = 0;
+            while (k < str.Length)
+            {
+                KeyValuePair<bool, int> resString = auto.MaxString(str, k);
+                if (resString.Key == true)
+                {
+                    result.Add(str.Substring(k, resString.Value));
+                    k += resString.Value;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+            auto.Show(result);
+        }
+    }
+}
